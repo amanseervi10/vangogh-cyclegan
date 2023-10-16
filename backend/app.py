c538716249b6dfe7d8d4ee965acb5344bb00389f
amanseervi10 <96574101+amanseervi10@users.noreply.github.com>
2023-10-16 17:55:43 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ import base64
 
 app = Flask(__name__)
 
-ort_session = onnxruntime.InferenceSession("model.onnx")
+# ort_session = onnxruntime.InferenceSession("model.onnx")
 
 @app.route('/')
 def index():
@@ -19,22 +19,22 @@ def vangoghify():
     input_data = np.array(request.json['input_data'], dtype=np.float32)
 
     # Perform inference
-    output = ort_session.run(None, {'input.1': input_data})
+    # output = ort_session.run(None, {'input.1': input_data})
     print("here also")
 
     # Process the output data (e.g., scaling, normalization)
-    output_image_data = output[0].squeeze() 
-    output_image_data = output_image_data.transpose(1, 2, 0)
-    output_image_data = (output_image_data * 0.5 + 0.5).clip(0.0, 1.0)
-    output_image_data = (output_image_data * 255.0).astype(np.uint8)
+    # output_image_data = output[0].squeeze() 
+    # output_image_data = output_image_data.transpose(1, 2, 0)
+    # output_image_data = (output_image_data * 0.5 + 0.5).clip(0.0, 1.0)
+    # output_image_data = (output_image_data * 255.0).astype(np.uint8)
 
     print(output_image_data.shape)
 
     # Prepare the output image for sending to the frontend
-    output_image = output_image_data.tobytes()
-    output_image_base64 = base64.b64encode(output_image).decode('utf-8')
+    # output_image = output_image_data.tobytes()
+    # output_image_base64 = base64.b64encode(output_image).decode('utf-8')
 
-    return jsonify({'output_image': output_image_base64})
+    # return jsonify({'output_image': output_image_base64})
 
 # if __name__ == '__main__':
 #     app.run(port=5000, debug=True)
